45f2f6196f73f66b1ebb7f3372830797968ec462
CYOAO <d1104183006@gm.lhu.edu.tw>
2023-10-18 10:58:49 +0800

    first commit

---
+++ database/migrations/2023_10_11_061658_create_areas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('element');
+            $table->string('belief');
+            $table->string('god');
+            $table->string('ruler');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
